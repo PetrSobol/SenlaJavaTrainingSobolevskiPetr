2ad41e452b90b8967a14acce5a309b4ad1d0243b
PetrSobol <petr.sobol1992@yandex.ru>
2017-01-18 14:20:25 +0200

    18.01.2017

--- task10/3/№2/name_DDL.sql
+++ task10/3/№2/name_DDL.sql
@@ -1,9 +1,9 @@
 -- MySQL Workbench Synchronization
--- Generated: 2017-01-17 22:53
+-- Generated: 2017-01-18 13:49
 -- Model: New Model
 -- Version: 1.0
 -- Project: Name of the project
--- Author: Sobol
+-- Author: ONRiGE
 
 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
@@ -11,9 +11,9 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
 CREATE TABLE IF NOT EXISTS `mydb`.`Customer` (
   `idCustomer` INT(11) NOT NULL AUTO_INCREMENT,
-  `lastnameCustomer` VARCHAR(45) NULL DEFAULT NULL,
-  `firstnameCustomer` VARCHAR(45) NULL DEFAULT NULL,
-  `phone` INT(11) NULL DEFAULT NULL,
+  `lastnameCustomer` VARCHAR(45) NOT NULL,
+  `firstnameCustomer` VARCHAR(45) NOT NULL,
+  `phone` INT(11) NOT NULL,
   PRIMARY KEY (`idCustomer`))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8;
@@ -21,12 +21,12 @@ DEFAULT CHARACTER SET = utf8;
 CREATE TABLE IF NOT EXISTS `mydb`.`Book` (
   `idBook` INT(11) NOT NULL AUTO_INCREMENT,
   `idWriter` INT(11) NOT NULL,
-  `nameBook` VARCHAR(45) NULL DEFAULT NULL,
-  `quantityBook` VARCHAR(45) NULL DEFAULT NULL,
+  `nameBook` VARCHAR(45) NOT NULL,
+  `quantityBook` VARCHAR(45) NOT NULL,
   `price` INT(11) NOT NULL,
-  PRIMARY KEY (`idBook`),
-  INDEX `fk_Book_Writer_idx` (`idWriter` ASC),
-  CONSTRAINT `fk_Book_Writer`
+  PRIMARY KEY (`idBook`, `idWriter`),
+  INDEX `fk_Book_Writer1_idx` (`idWriter` ASC),
+  CONSTRAINT `fk_Book_Writer1`
     FOREIGN KEY (`idWriter`)
     REFERENCES `mydb`.`Writer` (`idWriter`)
     ON DELETE NO ACTION
@@ -36,28 +36,27 @@ DEFAULT CHARACTER SET = utf8;
 
 CREATE TABLE IF NOT EXISTS `mydb`.`Writer` (
   `idWriter` INT(11) NOT NULL AUTO_INCREMENT,
-  `lastnameWriter` VARCHAR(45) NULL DEFAULT NULL,
-  `firstnameWriter` VARCHAR(45) NULL DEFAULT NULL,
-  `Start_year` DATE NULL DEFAULT NULL,
-  `Died_year` DATE NULL DEFAULT NULL,
+  `lastnameWriter` VARCHAR(45) NOT NULL,
+  `firstnameWriter` VARCHAR(45) NOT NULL,
+  `yearOfBirthday` DATE NULL DEFAULT NULL,
+  `diedYear` DATE NULL DEFAULT NULL,
   PRIMARY KEY (`idWriter`))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8;
 
 CREATE TABLE IF NOT EXISTS `mydb`.`Order` (
   `idCustomer` INT(11) NOT NULL,
-  `idBook` INT(11) NOT NULL,
-  `dateOrder` INT(11) NOT NULL,
-  PRIMARY KEY (`dateOrder`),
-  INDEX `fk_Order_Customer1_idx` (`idCustomer` ASC),
-  INDEX `fk_Order_Book1_idx` (`idBook` ASC),
-  CONSTRAINT `fk_Order_Customer1`
+  `dBook` INT(11) NOT NULL,
+  `dateOrder` DATE NOT NULL,
+  PRIMARY KEY (`idCustomer`, `dBook`),
+  INDEX `fk_Order_Book1_idx` (`dBook` ASC),
+  CONSTRAINT `fk_Order_Customer`
     FOREIGN KEY (`idCustomer`)
     REFERENCES `mydb`.`Customer` (`idCustomer`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_Order_Book1`
-    FOREIGN KEY (`idBook`)
+    FOREIGN KEY (`dBook`)
     REFERENCES `mydb`.`Book` (`idBook`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
